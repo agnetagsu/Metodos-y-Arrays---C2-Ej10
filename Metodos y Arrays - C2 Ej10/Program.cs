09d04b6969611bc0fee574929461e1a67f8c0858
Agneta <agnetagsu@yahoo.es>
2021-05-23 12:07:37 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] miarray = new int[11];
+            int numero;
+
+            Console.WriteLine("Escribe el numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < miarray.Length; i++)
+            {
+                miarray[i] = numero*i;
+            }
+            for (int i = 0; i < miarray.Length; i++)
+            {
+                Console.WriteLine(miarray[i]);
+            }
+        }
+    }
+}
